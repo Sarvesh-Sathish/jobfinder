c4ae43e500d841141125337a7c50a8fbdaefbb8e
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2025-08-22 14:25:10 -0400

    GENAI=YES Added PIV, Constructor, fetchCodeList in UsaJobsService

--- src/main/java/com/example/jobfinder/service/UsaJobsService.java
+++ src/main/java/com/example/jobfinder/service/UsaJobsService.java
@@ -1,26 +1,41 @@
 package com.example.jobfinder.service;
 
+import com.example.jobfinder.config.UsaJobsHeadersFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.http.HttpHeaders;
-
 @Service
 public class UsaJobsService {
-    @Value("${usajobs.api.url}")
-    private String apiUrl;
+    private UsaJobsHeadersFactory usaJobsHeadersFactory;
+    private final String apiUrl;
 
-    @Value("${usajobs.api.key}")
-    private String apiKey;
+    public UsaJobsService(UsaJobsHeadersFactory usaJobsHeadersFactory, @Value("${usajobs.api.url}") String apiUrl) {
+        this.usaJobsHeadersFactory = usaJobsHeadersFactory;
+        this.apiUrl = apiUrl;
+    }
 
-    @Value("${usajobs.user.agent}")
-    private String userAgent;
-
-    public String fetchJobsList(String type) {
+    public String fetchCodeList(String type) {
         RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = usaJobsHeadersFactory.createHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        return "";
+        String url = apiUrl + "/codelist/"  + type;
 
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                return response.getBody();
+            } else {
+                throw new RuntimeException("Failed to fetch codelist: " + response.getStatusCode());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Error fetching codelist from USAJobs API", e);
+        }
     }
 }
