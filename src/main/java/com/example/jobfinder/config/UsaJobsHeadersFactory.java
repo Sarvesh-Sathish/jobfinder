15e9fa60a02da0c45e6036d08922a833de3b9e5a
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2025-08-22 14:18:50 -0400

    GENAI=YES Implemented createHeaders() Function

--- src/main/java/com/example/jobfinder/config/UsaJobsHeadersFactory.java
+++ src/main/java/com/example/jobfinder/config/UsaJobsHeadersFactory.java
@@ -1,4 +1,20 @@
 package com.example.jobfinder.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+
+
 public class UsaJobsHeadersFactory {
+    @Value("${usajobs.api.key}")
+    private String apiKey;
+
+    @Value("${usajobs.user.agent}")
+    private String userAgent;
+
+    public HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization-Key", apiKey);
+        headers.set("User-Agent", userAgent);
+        return headers;
+    }
 }
