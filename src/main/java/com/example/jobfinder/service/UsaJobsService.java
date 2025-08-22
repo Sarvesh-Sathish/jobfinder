89d435dfe52ec9da498be5285deab06003e29c75
Sarvesh Sathish <sarveshsathish.2004@gmail.com>
2025-08-22 13:58:18 -0400

    Initial commit

---
+++ src/main/java/com/example/jobfinder/service/UsaJobsService.java
@@ -0,0 +1,9 @@
+package com.example.jobfinder.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsaJobsService {
+    private static final String API_URL = "https://data.usajobs.gov/api/codelist/";
+    private static final String API_KEY = "";
+}
